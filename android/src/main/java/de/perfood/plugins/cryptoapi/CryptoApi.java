0c8e2950ecdc12e797476c4d306a1918816b3982
Konstantinos Tsanakas <konstantinos.tsanakas@perfood.de>
2024-07-04 14:58:30 +0200

    add Android demo

--- android/src/main/java/de/perfood/plugins/cryptoapi/CryptoApi.java
+++ android/src/main/java/de/perfood/plugins/cryptoapi/CryptoApi.java
@@ -5,7 +5,32 @@ import android.util.Log;
 public class CryptoApi {
 
     public String echo(String value) {
-        Log.i("Echo", value);
+        Log.i("echo", value);
         return value;
+    }
+
+    public String generateKey(String tag, String algorithm) {
+        Log.i("generateKey", tag);
+        Log.i("generateKey", algorithm);
+        return tag;
+    }
+
+    public String loadKey(String tag) {
+        Log.i("loadKey", tag);
+        return tag;
+    }
+
+    public String sign(String tag, String data) {
+        Log.i("sign", tag);
+        Log.i("sign", data);
+        return tag;
+    }
+
+    public String decrypt(String tag, String foreignPublicKey, String iv, String data) {
+        Log.i("decrypt", tag);
+        Log.i("decrypt", foreignPublicKey);
+        Log.i("decrypt", iv);
+        Log.i("decrypt", data);
+        return tag;
     }
 }
--- android/src/main/java/de/perfood/plugins/cryptoapi/CryptoApiPlugin.java
+++ android/src/main/java/de/perfood/plugins/cryptoapi/CryptoApiPlugin.java
@@ -19,4 +19,45 @@ public class CryptoApiPlugin extends Plugin {
         ret.put("value", implementation.echo(value));
         call.resolve(ret);
     }
+
+    @PluginMethod
+    public void generateKey(PluginCall call) {
+        String tag = call.getString("tag");
+        String algorithm = call.getString("algorithm");
+
+        JSObject ret = new JSObject();
+        ret.put("publicKey", implementation.generateKey(tag, algorithm));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void loadKey(PluginCall call) {
+        String tag = call.getString("tag");
+
+        JSObject ret = new JSObject();
+        ret.put("publicKey", implementation.loadKey(tag));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void sign(PluginCall call) {
+        String tag = call.getString("tag");
+        String data = call.getString("data");
+
+        JSObject ret = new JSObject();
+        ret.put("signature", implementation.sign(tag, data));
+        call.resolve(ret);
+    }
+
+    @PluginMethod
+    public void decrypt(PluginCall call) {
+        String tag = call.getString("tag");
+        String foreignPublicKey = call.getString("foreignPublicKey");
+        String iv = call.getString("iv");
+        String encryptedData = call.getString("encryptedData");
+
+        JSObject ret = new JSObject();
+        ret.put("data", implementation.decrypt(tag, foreignPublicKey, iv, encryptedData));
+        call.resolve(ret);
+    }
 }
