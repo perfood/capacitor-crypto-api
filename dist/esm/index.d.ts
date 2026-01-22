48e7e6d5c1f2c0bbbd8eec51519331fc6689708a
Gina <gina.schlott@perfood.de>
2026-01-22 10:08:56 +0100

    remove dist folder + add dist to gitignore

--- dist/esm/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { CryptoApiPlugin } from './definitions';
-declare const CryptoApi: CryptoApiPlugin;
-export * from './definitions';
-export * from './utils';
-export { CryptoApi };
--- dist/esm/index.js
+++
@@ -1,8 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CryptoApi = registerPlugin('CryptoApi', {
-    web: () => import('./web').then((m) => new m.CryptoApiWeb()),
-});
-export * from './definitions';
-export * from './utils';
-export { CryptoApi };
-//# sourceMappingURL=index.js.map
